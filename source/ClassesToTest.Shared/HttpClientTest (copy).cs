1afd7da89628403543d2bfdcd4addd8a13ceea25
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-08 13:30:43 +0200

    folder restructuring

---
+++ source/ClassesToTest.Shared/HttpClientTest (copy).cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNETStandard10
+{
+    public class HttpClientTest
+    {
+        public HttpClientTest()
+        {
+            #if !NETSTANDARD1_0
+            System.Net.Http.HttpClient http_client = null;
+
+            http_client = new System.Net.Http.HttpClient();
+            #endif
+
+            return;
+        }
+    }
+}
