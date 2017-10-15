b6e9430fd278ceddf8cb2940de258b82f497da29
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-15 17:28:57 +0200

    samples n tests folders and solutions

---
+++ tests/NUnit.Tests.DotNETStandard10/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace NUnit.Tests.DotNETStandard10
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
