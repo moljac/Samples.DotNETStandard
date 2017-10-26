1b47ccdb3960e1a069727a0669a1a7a96a6821ec
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-26 22:49:24 +0200

    Data for tests (URIs)

--- tests/unit-tests/UnitTests.Common/Net/HTTP/Data.cs
+++ tests/unit-tests/UnitTests.Common/Net/HTTP/Data.cs
@@ -4,33 +4,29 @@ using System.Text;
 
 namespace UnitTests.Common.Net.HTTP
 {
+    /// <summary>
+    /// Data.
+    /// </summary>
     public partial class Data
     {
-        public static string UriAPIRequestBin
+        
+        public static Dictionary<string, string> UriAPIs
         {
             get
             {
-                return url_api;
+                return url_apis;
             }
             set
             {
-                url_api = value;
+                url_apis = value;
             }
         }
-        protected static string url_api = $"https://requestb.in/{id_requestbin}";
 
-        public static string IdRequestBin
+        protected static Dictionary<string, string> url_apis = new Dictionary<string, string>()
         {
-            get
-            {
-                return id_requestbin;
-            }
-            set
-            {
-                id_requestbin = value;
-            }
-        }
-        protected static string id_requestbin = "yxhhewyx";
-
+            {$"RequestBin",     $"https://requestb.in/108voy71"},
+            {$"POST server",    $"http://posttestserver.com/post.php?dir=holisticware-xamarin-auth"},
+            {$"PutsReq",        $"http://putsreq.com/WkUY3qJajJXXYtClyGeW"}
+        };
     }
 }
