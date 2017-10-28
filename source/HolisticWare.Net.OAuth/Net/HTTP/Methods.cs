f66ec4ad4bdc44346a10608f01c6564fb762e100
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-28 23:52:17 +0200

    methods/verbs for investigation

---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HolisticWare.Net.OAuth.Net.HTTP
+{
+    /// <summary>
+    /// HTTPM ethods.
+    /// </summary>
+    /// class   System.Net.Http.HttpMethod
+    /// class   System.Net.WebRequestMethods.Http
+    /// enum    System.Web.Mvc.HttpVerbs
+    /// enum    System.Net.Cache   
+    public enum Methods
+    {
+    }
+}
---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/Verbs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HolisticWare.Net.OAuth.Net.HTTP
+{
+    public class HTTPMethods
+    {
+        public HTTPMethods()
+        {
+        }
+    }
+}
