0a34bf6d0d5bdf3420a4c2ccd7e1fc9db780b6ca
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-04 21:52:26 +0100

    string and Uri extension methods

---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/System.String.ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolisticWare.Net.HTTP
+{
+    public static partial class StringExtensionMethods
+    {
+        public static Client AddHeaders(this string uris)
+        {
+            Client c = new Client();
+
+            return c;
+        }
+    }
+}
---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/System.Uri.ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HolisticWare.Net.HTTP
+{
+    public static partial class UriExtensionMethods
+    {
+        public static Client AddHeaders(this Uri uris)
+        {
+            Client c = new Client();
+
+            return c;
+        }
+    }
+}
