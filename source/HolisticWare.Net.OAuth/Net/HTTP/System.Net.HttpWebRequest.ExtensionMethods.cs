32a16fad08f8187aeb844f081bf69f7ae535ec83
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-04 21:53:10 +0100

    HttpWeb* extension methods

---
+++ source/HolisticWare.Net.OAuth/Net/HTTP/System.Net.HttpWebRequest.ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolisticWare.Net.HTTP
+{
+    public static partial class HttpWebRequestExtensionMethods
+    {
+        public static Client AddHeaders(this HttpWebRequest uris)
+        {
+            Client c = new Client();
+
+            return c;
+        }
+    }
+}
