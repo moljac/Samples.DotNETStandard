f6b926798cc7235523a07e341a355fe8cabd0a49
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-09 23:55:49 +0100

    JWT implementation initial commit

---
+++ source/HolisticWare.JSON/System/JSON/WebToken/JSON.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.JSON.WebToken
+{
+    /// <summary>
+    /// JSON Web Token.
+    /// Lightweight JSON Web Token class
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/system.identitymodel.tokens.jwtheader(v=vs.114).aspx"/>
+    public partial class Data
+    {
+        public Dictionary<string, string> Header
+        {
+            get;
+            set;
+        }
+
+        public Dictionary<string, object> Payload
+        {
+            get;
+            set;
+        }
+
+        public string Secret
+        {
+            get;
+            set;
+        }
+
+   }
+}
