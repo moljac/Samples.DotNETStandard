f29796fa43ff10c2dc3cf3eed3b07e3460311575
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-12 22:45:14 +0100

    removed files JSON

---
+++ tests/unit-tests/nunit/UnitTests.NUnit.Common/TestConfig.cs
@@ -0,0 +1,36 @@
+﻿#if XUNIT
+//https://xunit.github.io/docs/comparisons.html
+using Xunit;
+
+using Test              = Xunit.FactAttribute;
+
+// Aliases
+// dummy XUnit does need testFixture
+using TestFixture       = System.ObsoleteAttribute;  
+using TestFixtureSetUp  = System.ObsoleteAttribute;
+#endif
+
+#if NUNIT
+using NUnit.Framework;
+using Fact = NUnit.Framework.TestAttribute;
+#endif
+
+using System;
+
+namespace UnitTests.Common
+{
+    [SetUpFixture]
+    public class TestConfig
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var dir = System.IO.Path.GetDirectoryName(typeof(TestConfig).Assembly.Location);
+            Environment.CurrentDirectory = dir;
+
+            // or
+            System.IO.Directory.SetCurrentDirectory(dir);
+
+            return;
+        }
+    }}
