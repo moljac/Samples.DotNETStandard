9386ed59be544bad0b611220a11789213c968f13
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-28 22:22:31 +0100

    Moment implementation

--- source/HolisticWare.Core.Math.Statistics/IEnumerableExtensions.Moment.cs
+++ source/HolisticWare.Core.Math.Statistics/IEnumerableExtensions.Moment.cs
@@ -1,7 +1,101 @@
 ﻿using System;
-namespace HolisticWare.Core.Math.Statistics
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Math.Statistics
 {
-    public class IEnumerableExtensionsMoment
+    public static class IEnumerableExtensionsMoment
     {
+        public static double Median(this IEnumerable<short> x, int m)
+        {
+            double mean = x.Cast<int>().Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<ushort> x, int m)
+        {
+            double mean = x.Cast<int>().Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<int> x, int m)
+        {
+            double mean = x.Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<uint> x, int m)
+        {
+            double mean = x.Cast<long>().Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<long> x, int m)
+        {
+            double mean = x.Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<ulong> x, int m)
+        {
+            double mean = x.Cast<double>().Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
+        public static double Median(this IEnumerable<double> x, int m)
+        {
+            double mean = x.Average();
+            double sum = 0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+                sum += System.Math.Pow(x.ElementAt(i) - mean, m);
+
+            return sum / n;
+        }
+
     }
 }
