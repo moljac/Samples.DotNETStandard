4e8dc0a2057044194669a6d163676897ecacb807
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-29 14:02:28 +0100

    Kurtosis

---
+++ source/HolisticWare.Core.Math.Statistics/IEnumerableExtensions.Kurtosis.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Math.Statistics
+{
+    public static class IEnumerableExtensionsKurtosis
+    {
+        public static double Kurtosis(this IEnumerable<short> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<ushort> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<int> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<uint> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<long> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<ulong> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<float> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static double Kurtosis(this IEnumerable<double> x)
+        {
+            double m2 = x.Moment(2);
+            double m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0;
+        }
+
+        public static decimal Kurtosis(this IEnumerable<decimal> x)
+        {
+            decimal m2 = x.Moment(2);
+            decimal m4 = x.Moment(4);
+
+            return m4 / (m2 * m2) - 3.0m;
+        }
+
+    }
+}
