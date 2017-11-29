c5ab8c48a855c6ed88e331a39ba21e6b1118439f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-11-29 14:02:52 +0100

    KurtosisPop

---
+++ source/HolisticWare.Core.Math.Statistics/IEnumerableExtensions.KurtosisPop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Math.Statistics
+{
+    public static class IEnumerableExtensionsKurtosisPop
+    {
+    }
+}
